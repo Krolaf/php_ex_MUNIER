d88c603c2e5e49886f4704b580d1a2a251c1cc88
Krolaf <jerome@grafitoid.com>
2024-10-25 10:55:47 +0200

    zone_left+main_zone

--- home_control.php
+++ home_control.php
@@ -2,7 +2,8 @@
 $db = new data();
 
 // Ajouter une feuille de style
-$header = '<link rel="stylesheet" href="style.css">';
+$header = ' <link rel="stylesheet" href="style.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">';
 
 
 $sql = 'SELECT 
@@ -26,6 +27,29 @@ $page = '
     ' . $header . '
 </head>
 <body class="' . $mode . '">
+<main>
+<div class="admin-container">
+
+            <div class="card">
+                <h2>Titre</h2>
+                <p><span class="stat">ID:</span> <span class="stat-value"></span></p>
+                <p><span class="stat">HP:</span> <span class="stat-value"></span></p>
+                <p><span class="stat">Force:</span> <span class="stat-value"></span></p>
+                <p><span class="stat">Intelligence:</span> <span class="stat-value"></span></p>
+                <p><span class="stat">Endurance:</span> <span class="stat-value"></span></p>
+                <p><span class="stat">Dexterité:</span> <span class="stat-value"></span></p>
+                <div class="btn_zone">
+                    <button><i class="fas fa-user"></i></button>
+                    <button><i class="fas fa-cog"></i></button>
+                </div>
+            </div>
+</div>
+
+<div class="navbar">
+
+banane
+
+</div>
 
 <div class="listing-container">';
 
@@ -47,7 +71,11 @@ $page = '
         }
     }
 
-$page .= '</div>';
+$page .= '</div>
+
+</main>
+</body>
+';
 echo $page;
 
 ?>
